b98032e0c21dc9599ae8520b11b36b354a088061
OVAnechka <ocrio.box@gmail.com>
2016-11-29 16:59:57 +0300

    Корректно считываю файл
    Начал работать над выводом в чарт

--- TLM_parser/Form1.Designer.cs
+++ TLM_parser/Form1.Designer.cs
@@ -28,16 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.rtbMemoshka = new System.Windows.Forms.RichTextBox();
             this.btnParse = new System.Windows.Forms.Button();
             this.tbHeader = new System.Windows.Forms.TextBox();
+            this.tmrShow = new System.Windows.Forms.Timer(this.components);
+            this.chartShow = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartShow)).BeginInit();
             this.SuspendLayout();
             // 
             // rtbMemoshka
             // 
-            this.rtbMemoshka.Location = new System.Drawing.Point(12, 41);
+            this.rtbMemoshka.Location = new System.Drawing.Point(12, 380);
             this.rtbMemoshka.Name = "rtbMemoshka";
-            this.rtbMemoshka.Size = new System.Drawing.Size(824, 587);
+            this.rtbMemoshka.Size = new System.Drawing.Size(424, 248);
             this.rtbMemoshka.TabIndex = 0;
             this.rtbMemoshka.Text = "";
             // 
@@ -53,22 +60,47 @@
             // 
             // tbHeader
             // 
-            this.tbHeader.Location = new System.Drawing.Point(93, 14);
+            this.tbHeader.Location = new System.Drawing.Point(442, 380);
+            this.tbHeader.Multiline = true;
             this.tbHeader.Name = "tbHeader";
-            this.tbHeader.Size = new System.Drawing.Size(739, 20);
+            this.tbHeader.Size = new System.Drawing.Size(189, 248);
             this.tbHeader.TabIndex = 2;
+            // 
+            // tmrShow
+            // 
+            this.tmrShow.Interval = 30;
+            this.tmrShow.Tick += new System.EventHandler(this.tmrShow_Tick);
+            // 
+            // chartShow
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartShow.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartShow.Legends.Add(legend1);
+            this.chartShow.Location = new System.Drawing.Point(12, 41);
+            this.chartShow.Name = "chartShow";
+            series1.ChartArea = "ChartArea1";
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartShow.Series.Add(series1);
+            this.chartShow.Size = new System.Drawing.Size(619, 333);
+            this.chartShow.TabIndex = 3;
+            this.chartShow.Text = "chart1";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 640);
+            this.ClientSize = new System.Drawing.Size(645, 640);
+            this.Controls.Add(this.chartShow);
             this.Controls.Add(this.tbHeader);
             this.Controls.Add(this.btnParse);
             this.Controls.Add(this.rtbMemoshka);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmMain";
             this.Text = "TLM Parser";
+            ((System.ComponentModel.ISupportInitialize)(this.chartShow)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +111,8 @@
         private System.Windows.Forms.RichTextBox rtbMemoshka;
         private System.Windows.Forms.Button btnParse;
         private System.Windows.Forms.TextBox tbHeader;
+        private System.Windows.Forms.Timer tmrShow;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartShow;
     }
 }
 
--- TLM_parser/Form1.cs
+++ TLM_parser/Form1.cs
@@ -13,9 +13,40 @@ namespace TLM_parser
 {
     public partial class frmMain : Form
     {
+        private List<int[]> bigDataIn = new List<int[]>();
+        private int chartWidth = 128;
+        private int chartHeight = 4096;
+        private int tlmBlockSize = 65536;
+
+        private void chartInit()
+        {
+            int[] chartEnum = new int[128];
+
+            chartShow.ChartAreas[0].AxisY.Minimum = 0;
+            chartShow.ChartAreas[0].AxisY.Maximum = chartHeight;
+            chartShow.ChartAreas[0].AxisY.Interval = 128;
+            chartShow.ChartAreas[0].AxisX.Minimum = 0;
+            chartShow.ChartAreas[0].AxisX.Maximum = chartWidth;
+            chartShow.ChartAreas[0].AxisX.Interval = 16;
+            chartShow.Series["Series1"].IsVisibleInLegend = false;
+            chartShow.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartShow.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartShow.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+            chartShow.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chartShow.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chartShow.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chartShow.ChartAreas[0].CursorX.SelectionColor = Color.LightGray;
+            for (int i = 0; i < chartWidth; i++)
+            {
+                chartEnum[i] = i;
+                chartShow.Series["Series1"].Points.AddXY(chartEnum[i], 0);
+            }
+
+        }
         public frmMain()
         {
             InitializeComponent();
+            chartInit();
         }
 
         private void btnParse_Click(object sender, EventArgs e)
@@ -36,35 +67,71 @@ namespace TLM_parser
                      */
                     byte[] header = new byte[256];
                     for (int i = 0; i < 256; i++)
-                        header[i] = (byte)fsSource.ReadByte();
-                    tbHeader.Text = Encoding.Default.GetString(header);
-
-                    int startPtr = 256;
-                    int endPtr = 65792;
-                    int[] body = new int[32768];
-                    /*
-                     * Get first cycle values (with 32 byte header)
-                     */
-                    for (int i = startPtr; i < endPtr; i+=2)
+                    header[i] = (byte)fsSource.ReadByte();
+                    string[] args;
+                    args = Encoding.Default.GetString(header).Split('.');
+                    foreach (var item in args)
                     {
-                        body[(i - startPtr)/2] = (byte)fsSource.ReadByte();
-                        body[(i - startPtr)/2] += ((byte)fsSource.ReadByte() << 8);
+                        tbHeader.AppendText(item + "\r\n");
+                    }
+
+                    /*
+                     * Get cycle values (without 32 byte header)
+                     */
+                    int startPtr = 256 + 32;
+                    int endPtr = tlmBlockSize + startPtr;
+
+                    while (endPtr < fsSource.Length)
+                    {
+                        int[] body = new int[tlmBlockSize / 2];
+                        for (int i = startPtr; i < endPtr; i += 2)
+                        {
+                            body[(i - startPtr) / 2] = (byte)fsSource.ReadByte();
+                            body[(i - startPtr) / 2] += ((byte)fsSource.ReadByte() << 8);
+                        }
+                        bigDataIn.Add(body);
+                        startPtr = endPtr + 32;
+                        endPtr += tlmBlockSize + 32;
+                        for (int i = 0; i < 32; i++)
+                            fsSource.ReadByte();
                     }
                     /*
-                     * Print first cycle values (without 32 byte header)
+                     * enable timer to show values
                      */
-                    for (int i = 16; i < body.Length; i++)
+                    for (int i = 16; i < bigDataIn[0].Length; i++)
                     {
-                        if ((i % 16 == 0) && (i != 16))
+                        if ((i % 8 == 0) && (i != 16))
                         {
                             rtbMemoshka.AppendText("\r\n");
                         }
-                        rtbMemoshka.AppendText(body[i].ToString() + "\t");
+                        rtbMemoshka.AppendText(bigDataIn[0][i].ToString() + "\t");
                     }
-
+                    tmrShow.Enabled = true;
 
                 }
             }
+        }
+
+        private void tmrShow_Tick(object sender, EventArgs e)
+        {
+            //foreach (var cycle in bigDataIn)
+            //{
+                int cntGrp = 0;
+                int[] chartEnum = new int[1024];
+                int[] group = new int[1024];
+
+                for (int i = 0; i < group.Length; i++)
+                {
+                    group[i] = bigDataIn[0][i*cntGrp];// cycle[i*cntGrp];
+                }
+                cntGrp++;
+
+                for (int i = 0; i < 128; i++)
+                {
+                    chartEnum[i] = i;
+                    chartShow.Series["Series1"].Points.AddXY(chartEnum[i], group[i]);
+                }
+            //}
         }
     }
 }
