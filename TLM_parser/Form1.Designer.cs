06248ed9225793cb7b38bb6f3dd42740bc44b169
OVAnechka <ocrio.box@gmail.com>
2017-01-12 09:44:55 +0300

    Иногда даже правильные значения показывает. Убрать нули из массивов ИС, синхронизировать по маркерам

--- TLM_parser/Form1.Designer.cs
+++ TLM_parser/Form1.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.rtbMemoshka = new System.Windows.Forms.RichTextBox();
             this.btnParse = new System.Windows.Forms.Button();
             this.tbHeader = new System.Windows.Forms.TextBox();
             this.lbCycles = new System.Windows.Forms.ListBox();
             this.pbMain = new System.Windows.Forms.ProgressBar();
             this.lbDigital = new System.Windows.Forms.ListBox();
-            this.btnTest = new System.Windows.Forms.Button();
-            this.tbTest = new System.Windows.Forms.TextBox();
             this.rtbD0 = new System.Windows.Forms.RichTextBox();
             this.rtbD1 = new System.Windows.Forms.RichTextBox();
             this.rtbD2 = new System.Windows.Forms.RichTextBox();
             this.rtbD3 = new System.Windows.Forms.RichTextBox();
+            this.cHisto = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cDia = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnShowGraphic = new System.Windows.Forms.Button();
+            this.tmrDelay = new System.Windows.Forms.Timer(this.components);
+            this.tbTest = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cHisto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cDia)).BeginInit();
             this.SuspendLayout();
             // 
             // rtbMemoshka
@@ -93,24 +108,6 @@
             this.lbDigital.TabIndex = 5;
             this.lbDigital.SelectedIndexChanged += new System.EventHandler(this.lbDigital_SelectedIndexChanged);
             // 
-            // btnTest
-            // 
-            this.btnTest.Location = new System.Drawing.Point(1173, 266);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
-            this.btnTest.TabIndex = 6;
-            this.btnTest.Text = "test";
-            this.btnTest.UseVisualStyleBackColor = true;
-            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
-            // 
-            // tbTest
-            // 
-            this.tbTest.Location = new System.Drawing.Point(1173, 295);
-            this.tbTest.Multiline = true;
-            this.tbTest.Name = "tbTest";
-            this.tbTest.Size = new System.Drawing.Size(166, 52);
-            this.tbTest.TabIndex = 7;
-            // 
             // rtbD0
             // 
             this.rtbD0.Location = new System.Drawing.Point(12, 41);
@@ -147,17 +144,96 @@
             this.rtbD3.Text = "";
             this.rtbD3.Visible = false;
             // 
+            // cHisto
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.cHisto.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.cHisto.Legends.Add(legend5);
+            this.cHisto.Location = new System.Drawing.Point(12, 41);
+            this.cHisto.Name = "cHisto";
+            this.cHisto.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series11.ChartArea = "ChartArea1";
+            series11.IsValueShownAsLabel = true;
+            series11.Legend = "Legend1";
+            series11.LegendText = "12 bit data";
+            series11.Name = "s12";
+            series12.ChartArea = "ChartArea1";
+            series12.IsValueShownAsLabel = true;
+            series12.Legend = "Legend1";
+            series12.LegendText = "8 bit data";
+            series12.Name = "s08";
+            series13.ChartArea = "ChartArea1";
+            series13.IsValueShownAsLabel = true;
+            series13.Legend = "Legend1";
+            series13.LegendText = "Counters";
+            series13.Name = "Series";
+            this.cHisto.Series.Add(series11);
+            this.cHisto.Series.Add(series12);
+            this.cHisto.Series.Add(series13);
+            this.cHisto.Size = new System.Drawing.Size(1155, 290);
+            this.cHisto.TabIndex = 12;
+            this.cHisto.Text = "histo";
+            this.cHisto.Visible = false;
+            // 
+            // cDia
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.cDia.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.cDia.Legends.Add(legend6);
+            this.cDia.Location = new System.Drawing.Point(12, 337);
+            this.cDia.Name = "cDia";
+            this.cDia.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series14.Legend = "Legend1";
+            series14.Name = "Series12";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series15.Legend = "Legend1";
+            series15.Name = "Series8";
+            this.cDia.Series.Add(series14);
+            this.cDia.Series.Add(series15);
+            this.cDia.Size = new System.Drawing.Size(1155, 300);
+            this.cDia.TabIndex = 13;
+            this.cDia.Text = "dia";
+            this.cDia.Visible = false;
+            // 
+            // btnShowGraphic
+            // 
+            this.btnShowGraphic.Location = new System.Drawing.Point(1173, 337);
+            this.btnShowGraphic.Name = "btnShowGraphic";
+            this.btnShowGraphic.Size = new System.Drawing.Size(166, 79);
+            this.btnShowGraphic.TabIndex = 14;
+            this.btnShowGraphic.Text = "Show digital info sequentially";
+            this.btnShowGraphic.UseVisualStyleBackColor = true;
+            this.btnShowGraphic.Click += new System.EventHandler(this.btnShowGraphic_Click);
+            // 
+            // tmrDelay
+            // 
+            this.tmrDelay.Tick += new System.EventHandler(this.tmrDelay_Tick);
+            // 
+            // tbTest
+            // 
+            this.tbTest.Location = new System.Drawing.Point(1173, 290);
+            this.tbTest.Name = "tbTest";
+            this.tbTest.Size = new System.Drawing.Size(166, 20);
+            this.tbTest.TabIndex = 15;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1351, 640);
+            this.Controls.Add(this.tbTest);
+            this.Controls.Add(this.btnShowGraphic);
+            this.Controls.Add(this.cDia);
+            this.Controls.Add(this.cHisto);
             this.Controls.Add(this.rtbD3);
             this.Controls.Add(this.rtbD2);
             this.Controls.Add(this.rtbD1);
             this.Controls.Add(this.rtbD0);
-            this.Controls.Add(this.tbTest);
-            this.Controls.Add(this.btnTest);
             this.Controls.Add(this.lbDigital);
             this.Controls.Add(this.pbMain);
             this.Controls.Add(this.lbCycles);
@@ -167,6 +243,8 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmMain";
             this.Text = "TLM Parser";
+            ((System.ComponentModel.ISupportInitialize)(this.cHisto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cDia)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,12 +258,15 @@
         private System.Windows.Forms.ListBox lbCycles;
         private System.Windows.Forms.ProgressBar pbMain;
         private System.Windows.Forms.ListBox lbDigital;
-        private System.Windows.Forms.Button btnTest;
-        private System.Windows.Forms.TextBox tbTest;
         private System.Windows.Forms.RichTextBox rtbD0;
         private System.Windows.Forms.RichTextBox rtbD1;
         private System.Windows.Forms.RichTextBox rtbD2;
         private System.Windows.Forms.RichTextBox rtbD3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cHisto;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cDia;
+        private System.Windows.Forms.Button btnShowGraphic;
+        private System.Windows.Forms.Timer tmrDelay;
+        private System.Windows.Forms.TextBox tbTest;
     }
 }
 
--- TLM_parser/Form1.cs
+++ TLM_parser/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -10,10 +11,20 @@ namespace TLM_parser
     {
         private List<int[]> bigDataIn = new List<int[]>();
         private List<uint[]> bigDigitalInfo = new List<uint[]>();
+        private List<int> words = new List<int>();
         private int tlmBlockSize = 65536;
+        String fileName = "d:/digital.bin";
+        private int oldX;
 
-        private String M = "1111100110100100001010111011000";
-        private String B = "1111100110101";
+        private int HISTO_MIN = 0;
+        private int HISTO_YMAX = 4095;
+        private int HISTO_XMAX = 18;
+        private int HISTO_YINT = 256;
+        private int HISTO_XINT = 1;
+        private int GRAPH_MIN = 0;
+        private int GRAPH_YMAX = 4095;
+        private int GRAPH_XMAX = 1023;
+        private int GRAPH_YINT = 512;
 
         public frmMain()
         {
@@ -22,6 +33,8 @@ namespace TLM_parser
 
         private void btnParse_Click(object sender, EventArgs e)
         {
+            tmrDelay.Enabled = false;
+
             OpenFileDialog ofdSelectTLM = new OpenFileDialog();
 
             //ofdSelectTLM.InitialDirectory = "c:\\";
@@ -50,12 +63,6 @@ namespace TLM_parser
 
                     while (dataEnd < sourceFile.Length)
                     {
-                        int checkMarkerL = 0;
-                        int checkMarkerM = 0;
-                        int checkMarkerH = 0;
-                        bool writeDigital = false;
-                        int digitalCounter = 0;
-
                         int[] body = new int[tlmBlockSize / 2];
                         for (int i = dataStart; i < dataEnd; i += 2)
                         {
@@ -72,7 +79,6 @@ namespace TLM_parser
                     }
 
                     BinaryWriter dataOut;
-                    String fileName = "d:/digital.bin";
                     try
                     {
                         dataOut = new BinaryWriter(new FileStream(fileName, FileMode.Create));
@@ -114,13 +120,10 @@ namespace TLM_parser
                     }
                     dataOut.Close();
 
-                    double progress = 0.0f;
-                    double step = 100.0f / (65535);
                     if (File.Exists(fileName))
                     {
                         using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
                         {
-                            uint count = 0;
                             uint digitalCount = 0;
                             uint dat = 0;
                             uint[] digitalFrame = new uint[300];
@@ -156,6 +159,10 @@ namespace TLM_parser
                                     marker2 = digitalFrame[digitalCount - 1];
                                     marker1 = digitalFrame[digitalCount - 2];
                                     marker0 = digitalFrame[digitalCount - 3];
+                                    digitalFrame[digitalCount] = 0;
+                                    digitalFrame[digitalCount - 1] = 0;
+                                    digitalFrame[digitalCount - 2] = 0;
+                                    digitalFrame[digitalCount - 3] = 0;
                                     bigDigitalInfo.Add(digitalFrame);
                                     digitalFrame = new uint[300];
                                     lbDigital.Items.Add(String.Format("Frame #{0}", bigDigitalInfo.Count));
@@ -176,6 +183,9 @@ namespace TLM_parser
 
         private void lbCycles_SelectedIndexChanged(object sender, EventArgs e)
         {
+            tmrDelay.Enabled = false;
+            cDia.Visible = false;
+            cHisto.Visible = false;
             rtbMemoshka.Clear();
             rtbD0.Clear();
             rtbD1.Clear();
@@ -208,6 +218,9 @@ namespace TLM_parser
 
         private void lbDigital_SelectedIndexChanged(object sender, EventArgs e)
         {
+            tmrDelay.Enabled = false;
+            cDia.Visible = false;
+            cHisto.Visible = false;
             rtbMemoshka.Clear();
             rtbD0.Clear();
             rtbD1.Clear();
@@ -222,7 +235,6 @@ namespace TLM_parser
             int index;
             pbMain.Value = 0;
             int.TryParse(lbDigital.GetItemText(lbDigital.SelectedItem).Split(',')[0].Split('#')[1], out index);
-            double progress = 0.0f;
             double step = 100.0f / (bigDigitalInfo[index][0]);
 
             for (int i = 0; i < bigDigitalInfo[index].Length; i++)
@@ -241,17 +253,193 @@ namespace TLM_parser
             }
             pbMain.Value = 100;
             lbDigital.Enabled = true;
-//            Clipboard.SetText(rtbMemoshka.Text);
         }
 
-        private void btnTest_Click(object sender, EventArgs e)
+        private void Chart_Init()
         {
-            M.PadLeft(32, '0');
-            uint i;
-            i = Convert.ToUInt32(M);
+            int[] histogramEnum = new int[HISTO_XMAX + 1];
+            int[] graphicEnum = new int[GRAPH_XMAX + 1];
 
-            tbTest.Text = i.ToString() + "\r\n";
-            tbTest.AppendText((~i).ToString());
+            cHisto.ChartAreas[0].AxisY.Minimum = HISTO_MIN;
+            cHisto.ChartAreas[0].AxisY.Maximum = HISTO_YMAX + 1;
+            cHisto.ChartAreas[0].AxisY.Interval = HISTO_YINT;
+            cHisto.ChartAreas[0].AxisX.Minimum = HISTO_MIN;
+            cHisto.ChartAreas[0].AxisX.Maximum = HISTO_XMAX;
+            cHisto.ChartAreas[0].AxisX.Interval = HISTO_XINT;
+
+            cDia.ChartAreas[0].AxisY.Minimum = GRAPH_MIN;
+            cDia.ChartAreas[0].AxisY.Maximum = GRAPH_YMAX + 1;
+            cDia.ChartAreas[0].AxisY.Interval = GRAPH_YINT;
+            cDia.ChartAreas[0].AxisX.Minimum = GRAPH_MIN;
+            cDia.ChartAreas[0].AxisX.Maximum = GRAPH_XMAX;
+            cDia.ChartAreas[0].AxisX.Interval = GRAPH_YINT;
+
+            cHisto.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            cHisto.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            cHisto.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+            cHisto.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+
+            cDia.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            cDia.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            cDia.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+            cDia.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+
+            cHisto.ChartAreas[0].CursorX.IsUserEnabled = true;
+            cHisto.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            cHisto.ChartAreas[0].CursorX.SelectionColor = Color.LightGray;
+
+            cDia.ChartAreas[0].CursorX.IsUserEnabled = true;
+            cDia.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            cDia.ChartAreas[0].CursorX.SelectionColor = Color.Azure;
+
+            cDia.Series["Series12"].Points.Clear();
+            cDia.Series["Series8"].Points.Clear();
+            cHisto.Series["s12"].Points.Clear();
+            cHisto.Series["s08"].Points.Clear();
+            cHisto.Series["Series"].Points.Clear();
+
+            for (int i = 0; i < HISTO_XMAX + 1; i++)
+            {
+                histogramEnum[i] = i;
+                cHisto.Series["s12"].Points.AddXY(histogramEnum[i], 0);
+                cHisto.Series["s08"].Points.AddXY(histogramEnum[i], 0);
+                cHisto.Series["Series"].Points.AddXY(histogramEnum[i], 0);
+            }
+            for (int i = 0; i < GRAPH_XMAX + 1; i++)
+            {
+                cDia.Series["Series12"].Points.AddXY(graphicEnum[i], 0);
+                cDia.Series["Series8"].Points.AddXY(graphicEnum[i], 0);
+            }
+        }
+
+        private void btnShowGraphic_Click(object sender, EventArgs e)
+        {
+            cDia.Visible = true;
+            cHisto.Visible = true;
+            rtbMemoshka.Visible = false;
+            rtbD0.Visible = false;
+            rtbD1.Visible = false;
+            rtbD2.Visible = false;
+            rtbD3.Visible = false;
+            Chart_Init();
+            tmrDelay.Enabled = true;
+            if (File.Exists(fileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                {
+                    uint digitalCount = 0;
+                    uint dat = 0;
+                    uint[] digitalFrame = new uint[300];
+
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        dat = (uint)(reader.ReadByte() << 8);
+                        dat += (uint)(reader.ReadByte());
+                        uint marker0 = 0;
+                        uint marker1 = 0;
+                        uint marker2 = 0;
+                        uint marker3 = 0;
+                        digitalFrame[digitalCount] = dat;
+                        if ((digitalFrame[digitalCount] == 0x00CA)                                 //M ~B
+                            && (digitalFrame[digitalCount - 1] == 0x0760)
+                            && (digitalFrame[digitalCount - 2] == 0x010A)
+                            && (digitalFrame[digitalCount - 3] == 0x07CD) ||
+                            (digitalFrame[digitalCount] == 0x0735)                                 //M B
+                            && (digitalFrame[digitalCount - 1] == 0x0763)
+                            && (digitalFrame[digitalCount - 2] == 0x010A)
+                            && (digitalFrame[digitalCount - 3] == 0x07CD) ||
+                            (digitalFrame[digitalCount] == 0x0735)                                 //~M B
+                            && (digitalFrame[digitalCount - 1] == 0x009F)
+                            && (digitalFrame[digitalCount - 2] == 0x06F5)
+                            && (digitalFrame[digitalCount - 3] == 0x0032) ||
+                            (digitalFrame[digitalCount] == 0x00CA)                                 //~M ~B
+                            && (digitalFrame[digitalCount - 1] == 0x009C)
+                            && (digitalFrame[digitalCount - 2] == 0x06F5)
+                            && (digitalFrame[digitalCount - 3] == 0x0032)
+                            )
+                        {
+                            marker3 = digitalFrame[digitalCount];
+                            marker2 = digitalFrame[digitalCount - 1];
+                            marker1 = digitalFrame[digitalCount - 2];
+                            marker0 = digitalFrame[digitalCount - 3];
+                            digitalFrame[digitalCount] = 0;
+                            digitalFrame[digitalCount - 1] = 0;
+                            digitalFrame[digitalCount - 2] = 0;
+                            digitalFrame[digitalCount - 3] = 0;
+                            bigDigitalInfo.Add(digitalFrame);
+                            digitalFrame = new uint[300];
+                            lbDigital.Items.Add(String.Format("Frame #{0}", bigDigitalInfo.Count));
+                            digitalFrame[0] = marker0;
+                            digitalFrame[1] = marker1;
+                            digitalFrame[2] = marker2;
+                            digitalFrame[3] = marker3;
+                            digitalCount = 3;
+                        }
+                        digitalCount++;
+                        if (digitalCount == 299) digitalCount = 0;
+                    }
+                }
+                StringBuilder newbigstring = new StringBuilder();
+                for (int i = 1; i < bigDigitalInfo.Count; i++)
+                {
+                    uint[] work = bigDigitalInfo[i];
+                    for (int j = 4; j < work.Length; j++)
+                    {
+                        newbigstring.Append(Convert.ToString((work[j]), 2).PadLeft(11, '0'));
+                        if (newbigstring.Length > 16)
+                        {
+                            char[] arr = new char[16];
+                            newbigstring.CopyTo(0, arr, 0, 16);
+                            words.Add(Convert.ToInt32(new String(arr), 2) );
+                            newbigstring.Remove(0, 16);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void tmrDelay_Tick(object sender, EventArgs e)
+        {
+            cHisto.Series["s12"].Points.Clear();
+            cHisto.Series["s08"].Points.Clear();
+            cHisto.Series["Series"].Points.Clear();
+
+            cHisto.Series["s12"].Points.AddXY(0, words[oldX + 1] >> 4);
+            cHisto.Series["s12"].Points.AddXY(1, words[oldX + 2] >> 4);
+            cHisto.Series["s12"].Points.AddXY(2, words[oldX + 3] >> 4);
+            cHisto.Series["s12"].Points.AddXY(3, words[oldX + 4] >> 4);
+            cHisto.Series["s12"].Points.AddXY(4, words[oldX + 5] >> 4);
+            cHisto.Series["s12"].Points.AddXY(5, words[oldX + 6] >> 4);
+            cHisto.Series["s12"].Points.AddXY(6, words[oldX + 7] >> 4);
+            cHisto.Series["s12"].Points.AddXY(7, words[oldX + 8] >> 4);
+            cHisto.Series["s12"].Points.AddXY(8, words[oldX + 9] >> 4);
+
+            cHisto.Series["s12"].Points.AddXY(9, words[oldX + 11] >> 4);
+            cHisto.Series["s12"].Points.AddXY(10, words[oldX + 12] >> 4);
+            cHisto.Series["s12"].Points.AddXY(11, words[oldX + 13] >> 4);
+            cHisto.Series["s12"].Points.AddXY(12, words[oldX + 14] >> 4);
+            cHisto.Series["s12"].Points.AddXY(13, words[oldX + 15] >> 4);
+            cHisto.Series["s12"].Points.AddXY(14, words[oldX + 16] >> 4);
+            cHisto.Series["s12"].Points.AddXY(15, words[oldX + 17] >> 4);
+            cHisto.Series["s12"].Points.AddXY(16, words[oldX + 18] >> 4);
+            cHisto.Series["s12"].Points.AddXY(17, words[oldX + 19] >> 4);
+
+            cHisto.Series["s08"].Points.AddXY(0, (words[oldX + 11] & 0xF) + ((words[oldX + 1] & 0xF) << 4));
+            cHisto.Series["s08"].Points.AddXY(1, (words[oldX + 12] & 0xF) + ((words[oldX + 2] & 0xF) << 4));
+            cHisto.Series["s08"].Points.AddXY(2, (words[oldX + 13] & 0xF) + ((words[oldX + 3] & 0xF) << 4));
+            cHisto.Series["s08"].Points.AddXY(3, (words[oldX + 14] & 0xF) + ((words[oldX + 4] & 0xF) << 4));
+            cHisto.Series["s08"].Points.AddXY(4, (words[oldX + 15] & 0xF) + ((words[oldX + 5] & 0xF) << 4));
+            cHisto.Series["s08"].Points.AddXY(5, (words[oldX + 16] & 0xF) + ((words[oldX + 6] & 0xF) << 4));
+            cHisto.Series["s08"].Points.AddXY(6, (words[oldX + 17] & 0xF) + ((words[oldX + 7] & 0xF) << 4));
+
+            cHisto.Series["Series"].Points.AddXY(0, (words[oldX] >> 1) & 0x3F);
+            cHisto.Series["Series"].Points.AddXY(1, (words[oldX] >> 6));
+
+            cHisto.Series["Series"].Points.AddXY(9, (words[oldX + 10] >> 1) & 0x3F);
+            cHisto.Series["Series"].Points.AddXY(10, (words[oldX + 10] >> 6));
+            oldX += 10;
+
+            if (oldX > words.Count) tmrDelay.Enabled = false;
         }
     }
 }
